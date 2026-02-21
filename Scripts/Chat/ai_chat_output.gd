5f197284b67e4f7c750db3c498fdc139db66d27d
Oscar Ryley <145236180+Oscar-Ryley@users.noreply.github.com>
2026-02-21 17:07:54 +0000

    Separated Chat & Character
    Co-Authored-By: Rebecca Mewse <185494727+rmewse@users.noreply.github.com>

---
+++ Scripts/Chat/ai_chat_output.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var blue_box = preload("res://assets/Robots/blue_box.png")
+@onready var green_box = preload("res://assets/Robots/green_box.png")
+@onready var pink_box = preload("res://assets/Robots/pink_box.png")
+@onready var red_box = preload("res://assets/Robots/red_box.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Global.current_AI == "blue":
+		$AIBox.texture = blue_box
+	elif Global.current_AI == "green":
+		$AIBox.texture = green_box
+	elif Global.current_AI == "pink":
+		$AIBox.texture = pink_box
+	elif Global.current_AI == "red":
+		$AIBox.texture = red_box
